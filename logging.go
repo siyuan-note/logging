6224f958f554299a89114e1271e589737283d87e
Liang Ding <d@b3log.org>
2023-03-19 09:22:46 +0800

    :art: 文件系统读取或写入失败时退出内核 https://github.com/siyuan-note/siyuan/issues/7707

--- logging.go
+++ logging.go
@@ -35,10 +35,9 @@ const (
 	ExitCodeReadOnlyDatabase      = 20 // 数据库文件被锁
 	ExitCodeUnavailablePort       = 21 // 端口不可用
 	ExitCodeCreateConfDirErr      = 22 // 创建配置目录失败
-	ExitCodeBlockTreeErr          = 23 // 无法读写 blocktree.msgpack 文件
 	ExitCodeWorkspaceLocked       = 24 // 工作空间已被锁定
 	ExitCodeCreateWorkspaceDirErr = 25 // 创建工作空间失败
-	ExitCodeFileSysInconsistent   = 26 // 文件系统不一致
+	ExitCodeFileSysErr            = 26 // 文件系统错误
 	ExitCodeOk                    = 0  // 正常退出
 	ExitCodeFatal                 = 1  // 致命错误
 )
